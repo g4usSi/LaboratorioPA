80581658d8df25d7badc126fcc4b2fccc439c196
Geovanny <geovanny.alcon2@gmail.com>
2024-07-29 08:59:28 -0600

    Estructura base del programa tienda en linea

---
+++ Laboratorio1/Laboratorio1/OperacionesTienda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+
+    internal class OperacionesTienda
+    {
+        /*
+        public List<double> NombreProducto { get; set; }
+        public List<double> Precio { get; set; }
+        public OperacionesTienda (List<string> nombreProducto, List<double> precioProducto)
+        {
+            NombreProducto = new List<double>();
+            precioProducto = new List<double>();
+        }        
+        */
+        static int contadorProductos;
+        public double IngresarProductos() 
+        { 
+            
+            contadorProductos++;
+        }
+        public double Total() 
+        { 
+            
+        }
+        public double Descuento()
+        {
+
+        }
+        public bool Continuar() 
+        {
+            Console.WriteLine("Desea continuar agregando productos (si/no): ");
+            string continuar = Console.ReadLine();
+            if (continuar == "si" || continuar == "s")
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Laboratorio1;
+
+List<string> productos = new List<string>();
+List<double> precio = new List<double>();
+OperacionesTienda operaciones = new OperacionesTienda();
+
+do {
+    Console.WriteLine("...  Tienda en Linea ...");
+    Console.Write("Igrese Productos... ");
+
+} while (operaciones.Continuar());

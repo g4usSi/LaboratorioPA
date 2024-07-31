853d45259abfb8a40b0b2186ed4a5131c3150ec7
Geovanny <geovanny.alcon2@gmail.com>
2024-07-30 19:46:33 -0600

    Cambios generales

--- Programa 2/Programa 2/Producto.cs
+++ Programa 2/Programa 2/Producto.cs
@@ -7,28 +7,103 @@ using System.Threading.Tasks;
 namespace Programa_2
 {
     internal class Producto
-    {
+    { 
+        //los atributos no inician el tipo de dato
         public List<string> Nombre { get; set; }
         public List<double> Precio { get; set; }
         public List<int> Stock { get; set; }
+        //el constructor es este... y si inicializa el tipo de dato, lista
         public Producto()
-        { }
-
-
-        public void MostrarInformacionProducto() {
-            
-            Console.WriteLine($"Nombre: {Nombre}");
-            Console.WriteLine($"Precio: Q{Precio}");
-            Console.WriteLine($"Precio: Q{Stock}");
-        }
-        public void VenderProducto(int cantidadVendida) { 
-            Stock -= cantidadVendida;
-        }
-        public void ReestablecerStock(int cantidadReestablecida) {
-            Stock += cantidadReestablecida;
+        {
+            Nombre = new List<string>();
+            Precio = new List<double>();
+            Stock = new List<int>();
         }
 
-
-
+        public void AgregarProducto(string nombre, double precio, int stock)
+        {
+            if (nombre != "")
+            {
+                Nombre.Add(nombre);
+                Precio.Add(precio);
+                Stock.Add(stock);
+            }
+            else
+            {
+                Console.WriteLine("El nombre no puede quedar vacio...");
+            }
+        }
+        public int BuscarProducto()
+        {
+            Console.Write("Ingrese el nombre del producto: ");
+            string nombreIngresado = Console.ReadLine();
+            for (int i = 0; i < Stock.Count; i++)
+            {
+                if (Nombre[i].Equals(nombreIngresado))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void MostrarInformacionProducto()
+        {
+            int posicion = BuscarProducto();
+            if (posicion >= 0)
+            {
+                Console.WriteLine($"Nombre: {Nombre[posicion]}");
+                Console.WriteLine($"Precio: Q{Precio[posicion]}");
+                Console.WriteLine($"Stock: {Stock[posicion]}");
+            }
+            else
+            {
+                Console.WriteLine("Producto no encontrado.");
+            }
+        }
+        public void VenderProducto(int cantidadVendida) 
+        {
+            int posicion = BuscarProducto();
+            if (posicion >= 0)
+            {
+                Console.Write("Ingrese la cantidad que desea adquirir: ");
+                int cantidadReestablecida = Convert.ToInt32(Console.ReadLine());
+                Stock[posicion] += cantidadReestablecida;
+                Console.WriteLine("Stock actualizado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Producto no encontrado.");
+            }
+        }
+        public void ReestablecerStock()
+        {
+            int posicion = BuscarProducto();
+            if (posicion >= 0)
+            {
+                Console.Write("Ingrese la cantidad que desea reestablecer: ");
+                int cantidadReestablecida = Convert.ToInt32(Console.ReadLine());
+                Stock[posicion] += cantidadReestablecida;
+                Console.WriteLine("Stock actualizado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Producto no encontrado.");
+            }
+        }
+        public void ActualizarPrecioProducto() 
+        {
+            int posicion = BuscarProducto();
+            if (posicion >= 0)
+            {
+                Console.Write("Ingrese la cantidad que desea actualizar el precio: ");
+                int cantidadReestablecida = Convert.ToInt32(Console.ReadLine());
+                Stock[posicion] += cantidadReestablecida;
+                Console.WriteLine("Precio actualizado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Producto no encontrado.");
+            }
+        }
     }
 }
--- Programa 2/Programa 2/Program.cs
+++ Programa 2/Programa 2/Program.cs
@@ -1,4 +1,53 @@
 ﻿using Programa_2;
-List<Producto> productos = new List<Producto>();
+internal class Program
+{
+    static int numEntero = 0;
+    static double precio;
+    static int stock;
+    private static void Main(string[] args)
+    {
+        Producto productos = new Producto();
+        Console.WriteLine("Administrador de Productos...");
+        do
+        {
+            Menu();
 
-Console.WriteLine("Administrador de Productos...");
+            switch ()
+            {
+                case 1:
+                    Console.Write("Nombre: ");
+                    string nombre = Console.ReadLine();
+                    Console.Write("Precio: ");
+                    double precio = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Stock: ");
+                    int stock = Convert.ToInt32(Console.ReadLine());
+                    productos.AgregarProducto(nombre, precio, stock);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida...");
+                    break;
+            }
+        } while (numEntero != 6);
+
+        static void Menu()
+        {
+            Console.WriteLine("1. Crear un producto");
+            Console.WriteLine("2. Consultar informacion de un producto");
+            Console.WriteLine("3. Vender producto");
+            Console.WriteLine("4. Reestablecer Stock de un producto");
+            Console.WriteLine("5. Reestablecer Stock de un producto");
+            Console.WriteLine("6. Salir");
+        }
+        static void 
+
+
+    }
+}

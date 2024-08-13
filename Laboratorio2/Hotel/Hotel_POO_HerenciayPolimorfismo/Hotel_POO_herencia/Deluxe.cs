d6e20f306336a39dc35f1ed2578c1c06bceeae43
Geovanny Alcon <151603150+g4usSi@users.noreply.github.com>
2024-08-12 18:24:48 -0600

    Add files via upload

---
+++ Laboratorio2/Hotel/Hotel_POO_HerenciayPolimorfismo/Hotel_POO_herencia/Deluxe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_POO_herencia
+{
+    internal class Deluxe : Habitacion
+    {
+        public string ServiciosExtra { get; set; }
+        public Deluxe(int numeroHabitacion, double precioHabitacion, bool disponible, string clienteAsignado, string serviciosExtra)
+            : base(numeroHabitacion, precioHabitacion, disponible, clienteAsignado)
+        {
+            this.ServiciosExtra = serviciosExtra;
+        }
+        public Deluxe() { }
+        public override void AgregarHabitacion()
+        {
+            Console.WriteLine("\n\t\t\t> Agregar una habitacion deluxe <");
+            base.AgregarHabitacion();
+            Console.Write("Numero de habitaciones: ");
+            this.ServiciosExtra = Console.ReadLine();
+        }
+        public override void MostrarHabitacionUnica(Habitacion habitacion)
+        {
+            base.MostrarHabitacionUnica(habitacion);
+            if (habitacion is Deluxe habitacionDeluxe)
+            {
+                Console.WriteLine($"Número de habitaciones: {habitacionDeluxe.ServiciosExtra}");
+                Console.WriteLine("Tipo de habitacion: Deluxe");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Laboratorio2/Hotel/Hotel_POO_HerenciayPolimorfismo/Hotel_POO_herencia/Suite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_POO_herencia
+{
+    internal class Suite : Habitacion
+    {
+        public int Habitaciones { get; set; }
+        public bool Jacuzzi { get; set; }
+        public Suite(int numeroHabitacion, double precioHabitacion, bool disponible, string clienteAsignado, int numeroDeHabitaciones, bool jacuzzi)
+            : base(numeroHabitacion, precioHabitacion, disponible, clienteAsignado)
+        {
+            this.Habitaciones = numeroDeHabitaciones;
+            this.Jacuzzi = jacuzzi;
+        }
+        public Suite()
+        {
+            
+        }
+        public override void AgregarHabitacion()
+        {
+            Console.WriteLine("\n\t\t\t> Agregar una habitacion simple <");
+            base.AgregarHabitacion();
+            Console.Write("Numero de habitaciones: ");
+            this.Habitaciones = LlenarNumeroEntero();
+            Console.Write("Tiene Jacuzzi (Si/No): ");
+            string jacuzzi = Console.ReadLine().ToLower();
+            this.Jacuzzi = (jacuzzi == "si"||jacuzzi == "s");
+        }
+        public override void MostrarHabitacionUnica(Habitacion habitacion)
+        {
+            base.MostrarHabitacionUnica(habitacion);
+            if (habitacion is Suite habitacionSuite)
+            {
+                Console.WriteLine($"Número de habitaciones: {habitacionSuite.Habitaciones}");
+                Console.WriteLine($"Tiene Jacuzzi: {EnmascaradoTiene(habitacionSuite.Jacuzzi)}");
+                Console.WriteLine("Tipo de habitacion: Suite");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
